4f3162f5d8dccca8f7539d24918792cf28888955
Syed Ahmed <geeklycan@gmail.com>
2015-02-20 01:43:48 +0600

    Advanced Recent Post widget form build up done

--- widget/widget.advrpost.php
+++ widget/widget.advrpost.php
@@ -88,6 +88,8 @@ class Adv_Recent_Post_Widget extends WP_Widget {
         // all post type
         if(isset($instance['post_type_name'])){
             $post_type_name = $instance['post_type_name'];
+        }else{
+            $post_type_name = '';
         }
 
         // number of post display
@@ -104,8 +106,37 @@ class Adv_Recent_Post_Widget extends WP_Widget {
             $show_thumbs = 1;
         }
 
+        // 0 = vertical ; 1 = horizontal
+        if(isset($instance['display_style'])){
+            $display_style = $instance['display_style'];
+            if(0 == $display_style){
+                $vertical = true;
+                $horizontal = false;
+            }else{
+                $horizontal = true;
+                $vertical = false;
+            }
+        }else{
+            $vertical = true;
+            $horizontal = false;
+        }
 
+        // 1= yes ; 0 = no;
+        if(isset($instance['post_title_show'])){
+            $post_title_show = $instance['post_title_show'];
 
+            if( 1 == $post_title_show ){
+                $yes = true;
+                $no = false;
+            }else{
+                $yes = false;
+                $no = true;
+            }
+
+        }else{
+            $yes = true;
+            $no = false;
+        }
         ?>
         <p>
             <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
@@ -113,15 +144,73 @@ class Adv_Recent_Post_Widget extends WP_Widget {
         </p>
 
         <p>
-            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+            <label for="<?php echo $this->get_field_id('post_type_name'); ?>"><?php _e( 'Post Type: '); ?>
+                <select class='widefat' id="<?php echo $this->get_field_id('post_type_name'); ?>"
+                        name="<?php echo $this->get_field_name('post_type_name'); ?>" >
+
+                    <?php
+                    $post_types = $this->get_all_post_type();
+                    foreach($post_types AS $post_type):
+                        if("attachment" != $post_type):
+                    ?>
+                    <option value='<?php echo $post_type; ?>'<?php echo ($post_type_name == $post_type)?'selected':''; ?>>
+                        <?php echo $post_type; ?>
+                    </option>
+
+                    <?php
+                        endif;
+                        endforeach; ?>
+
+                </select>
+            </label>
         </p>
 
+        <p>
+            <label for="<?php echo $this->get_field_id( 'number_of_post' ); ?>"><?php _e( 'Number Of Post:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'number_of_post' ); ?>" name="<?php echo $this->get_field_name( 'number_of_post' ); ?>" type="text" value="<?php echo esc_attr( $number_of_post ); ?>" />
+        </p>
+
+        <p>
+            <input class="checkbox" type="checkbox" <?php checked($show_thumbs, 1); ?> id="<?php echo $this->get_field_id('show_thumbs'); ?>" name="<?php echo $this->get_field_name('show_thumbs'); ?>" />
+            <label for="<?php echo $this->get_field_id('show_thumbs'); ?>"><?php _e( 'Show Thumbnails: '); ?></label>
+        </p>
+
+
+
+        <p>
+           <label for="<?php echo $this->get_field_id( 'display_style' ); ?>"><?php _e('Display Style: '); ?></label>
+           <label>
+               <input type="radio" name="<?php echo $this->get_field_name('display_style'); ?>" id="<?php $this->get_field_name('display_style'); ?>" value="0" <?php checked($vertical, true); ?>/>
+        <?php _e( 'Vertical'); ?>
+           </label>
+            <label>
+               <input type="radio" name="<?php echo $this->get_field_name('display_style'); ?>" id="<?php $this->get_field_name('display_style'); ?>" value="0" <?php checked($horizontal, true); ?>/>
+        <?php _e( 'Horizontal'); ?>
+           </label>
+        </p>
+
+
+
+        <p>
+            <label for="<?php echo $this->get_field_id( 'post_title_show' ); ?>"><?php _e('Show Post Title: '); ?></label>
+            <label>
+                <input type="radio" name="<?php echo $this->get_field_name('post_title_show'); ?>" id="<?php $this->get_field_name('post_title_show'); ?>" value="1" <?php checked($yes, true); ?>/>
+                <?php _e( 'Yes'); ?>
+            </label>
+            <label>
+                <input type="radio" name="<?php echo $this->get_field_name('post_title_show'); ?>" id="<?php $this->get_field_name('post_title_show'); ?>" value="0" <?php checked($no, true); ?>/>
+                <?php _e( 'No'); ?>
+            </label>
+        </p>
 
 
     <?php
     }
 
+    private function get_all_post_type(){
+        $args = array('public' => true);
+        return get_post_types($args, 'names');
+    }
 } // class Adv_Widget
 
 
